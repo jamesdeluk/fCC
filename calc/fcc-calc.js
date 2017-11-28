a24e14d91e09a1de81f0cb12e6d9aafb9913f142
James <jgibbins92@gmail.com>
2017-11-28 17:49:59 +0900

    final before string-first method

--- calc/fcc-calc.js
+++ calc/fcc-calc.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 let inputArr = [];
 
 const numButtons = Array.from(document.getElementsByClassName("button-input"));
@@ -13,10 +14,9 @@ clearButton.addEventListener("click", clear);
 backButton.addEventListener("click", back);
 
 function inputFunc() {
-  // console.log(this.classList.contains('button-number'));
   if (inputArr[inputArr.length-1] === undefined) {
     inputArr.push(this.innerText);
-  } else if (this.classList.contains('button-number')) {
+  } else if (this.classList.contains("button-number")) {
     inputArr[inputArr.length-1] = inputArr[inputArr.length-1] + this.innerText;
   } else {
     inputArr.push(this.innerText);
@@ -39,8 +39,12 @@ function clear() {
 }
 
 function back() {
-  inputArr = inputArr.slice(0,inputArr.length-1)
+  // let last = inputArr[inputArr.length-1];
+  // last = last.slice(0, last.length-1);
+  // inputArr[inputArr.length-1] = last;
   // console.log(inputArr);
+  let input = inputArr.join("Z");
+  
   display();
 }
 
--- calc/fcc-calc.md
+++ calc/fcc-calc.md
@@ -5,7 +5,8 @@ User Story: I can clear the input field with a clear button.
 User Story: I can keep chaining mathematical operations together until I hit the equal button, and the calculator will tell me the correct output.
 
 #IDEAS
-- Create arr of inputs then loop through array (clear empties arr)
+- Create arr of inputs then loop through array?
+- Create temp var on number press, add to array on action press?
 
 #NOTES
 - Be aware of BODMAS
